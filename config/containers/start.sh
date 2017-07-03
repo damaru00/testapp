e1906075f13455baef0bc08e729554c710a810b6
damaru00 <jesusjoeymayap@gmail.com>
2017-07-03 13:45:22 +0800

    test

--- config/containers/start.sh
+++ config/containers/start.sh
@@ -4,5 +4,7 @@ bundle check || bundle install
 
 # Start apache
 #service apache2 start
-source /etc/apache2/envvars
-apache2 -D FOREGROUND
+#source /etc/apache2/envvars
+#apache2 -D FOREGROUND
+
+pm2-docker start all
